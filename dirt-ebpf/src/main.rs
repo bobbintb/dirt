1c859596e8e4a8e3e6dd06a2c42b85bc40de7360
Armando Anglesey <bobbintb@gmail.com>
2025-09-29 19:43:11 -0700

    Initial commit

---
+++ dirt-ebpf/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{macros::uprobe, programs::ProbeContext};
+use aya_log_ebpf::info;
+
+#[uprobe]
+pub fn dirt(ctx: ProbeContext) -> u32 {
+    match try_dirt(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_dirt(ctx: ProbeContext) -> Result<u32, u32> {
+    info!(&ctx, "function 0xdf6a called by /usr/libexec/unraid/shfs");
+    Ok(0)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(link_section = "license")]
+#[unsafe(no_mangle)]
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
